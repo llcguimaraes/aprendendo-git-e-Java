39dea0a21153d8c70f994d2cd28a1ea76dc11321
Adriano de Mello Rodrigues <adriano.rodrigues@celk.net>
2020-02-13 21:29:24 -0300

    adicionado array para exercicios

---
+++ src/main/java/util/ArrayPessoasUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayPessoasUtil {
+    static ArrayList<String> pessoas = new ArrayList<String>(
+            Arrays.asList(new String[]{
+                    "Adriano", "Leo", "Joao", "Frango", "Ed", "Achacs", "Lucas"
+            })
+    );
+}
