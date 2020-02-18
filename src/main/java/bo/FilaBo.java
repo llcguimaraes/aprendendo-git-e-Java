2a21090338fa353b1c17620bd36668e48e379d78
unknown <leonardo>
2020-02-18 19:05:02 -0300

    Fila e Lista

---
+++ src/main/java/bo/FilaBo.java
@@ -0,0 +1,12 @@
+package bo;
+
+import java.util.ArrayList;
+
+public class FilaBo {
+    public static void fila(ArrayList<String> vetor) {
+        for (String palavra : vetor) {
+            System.out.println(palavra);
+        }
+    }
+
+}
