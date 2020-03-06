56922ee04cd09f2cca06c9e28616e9b1ae6b3a65
leonardo <llcguimaraes@hotmail.com>
2020-03-06 16:11:05 -0300

    wip reserva de poltronas (falta só a matriz)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import bo.ContaLetrasBo;
 import bo.FilaBo;
 import bo.ListaBo;
+import bo.ReservaPoltronaBo;
+import sun.font.TrueTypeFont;
 import util.ArrayPessoasUtil;
 
 import java.util.ArrayList;
@@ -12,23 +14,62 @@ public class Main {
     public static int matrizcontrole[][] = new int[12][6];
 
     public static void main(String[] args) {
-        //ArrayList<String> vetor = ArrayPessoasUtil.pessoas;
+        /*
+        ArrayList<String> vetor = ArrayPessoasUtil.pessoas;
 
-        //FilaBo.fila(vetor);
-        //ListaBo.lista(vetor);
+        FilaBo.fila(vetor);
+        ListaBo.lista(vetor);
 
-       // String frase = "Hello World!";
-        //Map<Character, Integer> map = ContaLetrasBo.contaletras(frase);
-        //System.out.println(map);
-
+        String frase = "Hello World!";
+        Map<Character, Integer> map = ContaLetrasBo.contaletras(frase);
+        System.out.println(map);
+        */
         for (int i=0; i<12; i++){
             for (int j=0; j<6; j++){
                 matrizcontrole[i][j] = 0;
             }
         }
 
-        Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
+        while (true){
+            int fila;
+            int assento;
+
+            while (true){
+                System.out.println("Qual fila deseja?");
+                Scanner scannerfila = new Scanner(System.in);
+                fila = scannerfila.nextInt();
+                if (fila > 6){
+                    System.out.println("Existem apenas 6 filas, escolha novamente.");
+                }
+                else{
+                    break;
+                }
+            }
+
+            while (true){
+                System.out.println("Qual assento deseja?");
+                Scanner scannerassento = new Scanner(System.in);
+                assento = scannerassento.nextInt();
+                if (assento > 12 ){
+                    System.out.println("Existem apenas 12 assentos por fila, escolha novamente.");
+                }
+                else{
+                    break;
+                }
+            }
+
+            String resultado = ReservaPoltronaBo.reservapoltrona(fila, assento);
+            System.out.println(resultado);
+
+            System.out.println("Deseja reservar outra poltrona? [s/n]");
+            Scanner scannermenu = new Scanner(System.in);
+            char variavelmenu = scannermenu.next().charAt(0);
+
+            if (variavelmenu == 'n'){
+                break;
+            }
+        }
+
 
 
     }
