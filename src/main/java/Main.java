fc33106f480262d45aea10ab1c841bba7da2183e
leonardo <llcguimaraes@hotmail.com>
2020-03-06 10:15:31 -0300

    wip reserva de poltronas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,32 @@ import util.ArrayPessoasUtil;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class Main {
+    public static int matrizcontrole[][] = new int[12][6];
+
     public static void main(String[] args) {
         //ArrayList<String> vetor = ArrayPessoasUtil.pessoas;
 
         //FilaBo.fila(vetor);
         //ListaBo.lista(vetor);
 
-        String frase = "Hello World!";
-        Map<Character, Integer> map = ContaLetrasBo.contaletras(frase);
-        System.out.println(map);
+       // String frase = "Hello World!";
+        //Map<Character, Integer> map = ContaLetrasBo.contaletras(frase);
+        //System.out.println(map);
+
+        for (int i=0; i<12; i++){
+            for (int j=0; j<6; j++){
+                matrizcontrole[i][j] = 0;
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+
     }
 }
 
---
+++ src/main/java/bo/ReservaPoltronaBo.java
@@ -0,0 +1,17 @@
+package bo;
+
+public class ReservaPoltronaBo {
+    public static String reservapoltrona(int fila, int assento){
+        String resultado = "asd";
+        /*
+        if (matriz[fila-1][assento-1] == 0){
+            matriz[fila-1][assento-1] = 1;
+            resultado = "Assento reservado com sucesso";
+        }
+        else {
+            resultado = "Esse assento já foi reservado";
+        }
+        */
+        return resultado;
+    }
+}
