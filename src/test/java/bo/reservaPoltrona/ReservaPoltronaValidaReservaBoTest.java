79576adea32acae953f73e76d439336d3df2edcf
Adriano de Mello Rodrigues <adriano.rodrigues@celk.net>
2020-03-10 18:47:47 -0300

    Configurando testes

---
+++ src/test/java/bo/reservaPoltrona/ReservaPoltronaValidaReservaBoTest.java
@@ -0,0 +1,16 @@
+package bo.reservaPoltrona;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReservaPoltronaValidaReservaBoTest {
+
+    @Test
+    public void validaFila() {
+    }
+
+    @Test
+    public void validaAssento() {
+    }
+}
